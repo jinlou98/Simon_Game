d3a5c97b48cbb166b379371fd8a83aa8a33bcb0f
Jin Lou <jinlou@bu.edu>
2021-06-13 12:45:36 -0400

    Add click action and click animation.

--- Simon Game/index.js
+++ Simon Game/index.js
@@ -1,6 +1,6 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
-
+var userClickedPattern = [];
 
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
@@ -11,4 +11,27 @@ function nextSequence() {
 
   var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
   audio.play();
+}
+
+$(".btn").on("click",function(){
+  var userChosenColour = this.id;
+  userClickedPattern.push(userChosenColour);
+
+  var audio = new Audio("sounds/" + this.id + ".mp3");
+  audio.play();
+
+  animatePress(userChosenColour);
+  console.log(userClickedPattern);
+
+});
+
+
+function animatePress(currentColour) {
+
+$("#"+currentColour).addClass("pressed");
+
+setTimeout(function(){
+  $("#"+currentColour).removeClass("pressed");
+},100);
+
 }
